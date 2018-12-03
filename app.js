5499b11be31202d151b41a166524f8c063ae5eca
Kaan Caliskan <kaancaliskan@Kaans-MacBook-Air.local>
2018-12-03 10:59:47 -0800

    changed data object's email property

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const http = new EasyHTTP();
 
-
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
-.then(data => console.log(data))
-.catch(err => console.log(err))
-
+http
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
 //User Data
 const data = {
-  name:'adam smith',
-  username:'asmith',
-  email:'asmith.com'
-}
+  name: "adam smith",
+  username: "asmith",
+  email: "adamsmith@gmail.com"
+};
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/users', data)
-.then(data => console.log(data))
-.catch(err => console.log(err));
+http
+  .post("https://jsonplaceholder.typicode.com/users", data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
